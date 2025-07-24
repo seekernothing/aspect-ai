3a8851813607da71a86ee76b1bb934ea930b824c
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-07-24 13:35:49 +0530

    remove commented-out route matcher for API video in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher([
   "/video-upload(.*)",
   "/social-share(.*)",
+  // "/api/video(.*)",
 ]);
 
 const isPublicRoute = createRouteMatcher([
